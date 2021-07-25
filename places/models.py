3ea09013aaf021d00bcf9c8551b1e5779bc861c9
Nurzat <nurje.love@mail.ru>
2021-07-25 21:50:24 +0600

    add user fk in place model

--- places/models.py
+++ places/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Place(models.Model):
+    user = models.ForeignKey(
+        to=User, on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null = True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     views_count = models.IntegerField(default=0)
