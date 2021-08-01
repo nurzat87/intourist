2905afb1600dc73dcc42469e9363367b984b6409
Nurzat <nurje.love@mail.ru>
2021-08-01 12:48:28 +0600

    add forms, crud based on fbv

---
+++ places/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Place
+
+
+class PlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ['name', 'location', 'description']
+
